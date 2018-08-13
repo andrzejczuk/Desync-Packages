2a7e4417d28115582a679ea28435d3b499a7d567
Andrzej Andrzejczuk <andrzej.andrzejczuk@outlook.com>
2018-08-13 08:48:14 +0200

    wrap to function

--- Desync-Packages.ps1
+++ Desync-Packages.ps1
@@ -1,1 +1,5 @@
-get-package | group-object Id | %{$versions = ($_.Group | Group-object Version);if(($versions | measure-object).Count -gt 1){$versions;};} | %{$_.Group}
+function Desync-Packages () {
+    get-package | group-object Id | %{$versions = ($_.Group | Group-object Version);if(($versions | measure-object).Count -gt 1){$versions;};} | %{$_.Group}
+}
+
+Export-ModuleMember -Function 'Desync-Packages'
