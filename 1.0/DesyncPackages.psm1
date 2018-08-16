392181a9d974d6e15debb1053af60c59ee141465
Andrzej Andrzejczuk <andrzej.andrzejczuk@ceneo.pl>
2018-08-16 13:39:18 +0200

    branch packages

--- 1.0/DesyncPackages.psm1
+++ 1.0/DesyncPackages.psm1
@@ -2,4 +2,9 @@ function Desync-Packages () {
     get-package | group-object Id | %{$versions = ($_.Group | Group-object Version);if(($versions | measure-object).Count -gt 1){$versions;};} | %{$_.Group}
 }
 
+function Branch-Packages () {
+    get-package | group-object Id | %{$versions = ($_.Group | Group-object Version);if(($versions.Name -match '[a-zA-Z]')) { $versions; };} | %{$_.Group}
+}
+
 Export-ModuleMember -Function 'Desync-Packages'
+Export-ModuleMember -Function 'Branch-Packages'
